bdafffc81dc1c58b77f5f6fc8f6e372b2de15447
Michael Doyle <doyle.michael1999@gmail.com>
2022-08-27 11:49:34 +1000

    appbar + bottombar + bg image

---
+++ lib/app_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ProfileAppBar extends StatefulWidget implements PreferredSizeWidget {
+  ProfileAppBar({Key? key})
+      : preferredSize = Size.fromHeight(kToolbarHeight),
+        super(key: key);
+
+  @override
+  final Size preferredSize; // default is 56.0
+
+  @override
+  State<ProfileAppBar> createState() => _ProfileAppBarState();
+}
+
+class _ProfileAppBarState extends State<ProfileAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            'images/quests_logo.png',
+            height: 56,
+          ),
+          Text('Profile Name: Lvl x'),
+        ],
+      ),
+      // bottom: TabBar(tabs: [
+      //   Tab(
+      //     icon: Icon(Icons.psychology),
+      //   ),
+      //   Tab(
+      //     icon: Icon(Icons.menu_book),
+      //   ),
+      //   Tab(
+      //     icon: Icon(Icons.account_box),
+      //   ),
+      // ]),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:hackathon_app/profile.dart';
-import 'package:hackathon_app/quests.dart';
-import 'package:hackathon_app/side_quests.dart';
+import 'package:hackathon_app/app_bar.dart';
+import 'package:hackathon_app/main_quest_screen/quests.dart';
+import 'package:hackathon_app/profile_screen/profile.dart';
+import 'package:hackathon_app/side_quest_screen/side_quests.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,27 +30,93 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hackathon Project'),
-      ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, "/quests");
-          },
-          onDoubleTap: () {
-            Navigator.pushNamed(context, '/side_quests');
-          },
-          onLongPress: () {
-            Navigator.pushNamed(context, '/profile');
-          },
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
+      appBar: ProfileAppBar(),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/osrs_background.png'),
+            fit: BoxFit.cover,
           ),
         ),
+        child: Column(
+          children: [],
+        ),
+        // child: GestureDetector(
+        //   onTap: () {
+        //     Navigator.pushNamed(context, "/quests");
+        //   },
+        //   onDoubleTap: () {
+        //     Navigator.pushNamed(context, '/side_quests');
+        //   },
+        //   onLongPress: () {
+        //     Navigator.pushNamed(context, '/profile');
+        //   },
+        //   child: Container(
+        //     width: 100,
+        //     height: 100,
+        //     color: Colors.red,
+        //   ),
+        // ),
       ),
+      bottomNavigationBar: BottomNavBar(),
+    );
+  }
+}
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.business),
+          label: 'Business',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.school),
+          label: 'School',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
     );
   }
 }
